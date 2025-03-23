b65dc00c7b87154935234f2eb1792bf054e0bd3a
Bhavya Ashok <60926204+bhavya478@users.noreply.github.com>
2025-03-23 10:33:14 -0500

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,16 +4,6 @@ const Footer = () => {
 			<div className='flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row'>
 				<p className='text-balance text-center text-sm leading-loose text-muted-foreground md:text-left'>
 					Under Development ♥️
-					The source code is available on{" "}
-					<a
-						href='https://github.com/bhavya478'
-						target='_blank'
-						rel='noreferrer'
-						className='font-medium underline underline-offset-4'
-					>
-						GitHub
-					</a>
-					.
 				</p>
 			</div>
 		</footer>
